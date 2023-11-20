f83ba32fd0994b3f0d1e0466d4f3d7f662bf25a2
lapinRagnar <lapinragnar@gmail.com>
2023-11-20 09:37:34 +0100

    reparer le server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors())
-app.use(express)
+app.use(express.json({limit: '50mb'}))
 
 app.get('/', (req, res) => {
   res.status(200).json({message: 'Hello from DALL.E'})
